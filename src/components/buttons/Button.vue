275679ccbf893dca57bfb9bfb407e1def764dcd4
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2024-08-09 22:23:47 +0700

    styling: create home page

---
+++ src/components/buttons/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button :class="{ [buttonBlue]: buttonType === 'blue' }">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    click: {
+      type: Function,
+    },
+    buttonType: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      buttonBlue:
+        "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-5/6",
+    };
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div className="flex flex-col items-center justify-center p-3 h-[97vh]">
+    <!-- Illustration -->
+    <img src="../assets/images/illustration.jpg" alt="illustration" class="w-5/6" />
+    <!-- Illustration -->
+
+    <!-- Kickstart -->
+    <div className="text-center mb-10">
+      <p className="text-2xl font-bold mb-7">
+        Keep On Track <br />
+        With MyTodo
+      </p>
+      <p className="text-sm text-gray-400 italic">
+        Make Your Life Easier and Organized
+      </p>
+    </div>
+    <!-- Kickstart -->
+
+    <!-- Button Next -->
+    <Button buttonType="blue">
+        Let's Go !
+    </Button>
+    <!-- Button Next -->
+  </div>
+</template>
+
+<script>
+import Button from "../components/buttons/Button.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Button,
+  },
+};
+</script>
